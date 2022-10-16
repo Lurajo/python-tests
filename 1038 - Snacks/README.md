469ef4988aa2c6db3f62604c2cbe3a9571b6c157
Lucas Araujo <93217820+Lurajo@users.noreply.github.com>
2022-10-15 21:03:58 -0300

    Finish Test_UserInterface.py

--- 1038 - Snacks/README.md
+++ 1038 - Snacks/README.md
@@ -1,4 +1,4 @@
 ## Testing Snacks
-For future implementation of tests for the functions on [Snacks].
+A series of tests for the functions on [Snacks].
 
 [Snacks]: https://github.com/jeftegoes/BeecrowdExercices/tree/master/1038%20-%20Snacks
--- 1038 - Snacks/Test_UserInterface.py
+++ 1038 - Snacks/Test_UserInterface.py
@@ -1,20 +1,39 @@
+from io import StringIO
+from sys import stdin
+
 from Menu import Menu
 from MenuRepository import MenuRepository
 from Order import Order
+from UserInterface import UserInterface
 
 
 def test_get_total_price():
     # Arrange
     menu_repository = MenuRepository()
     menu1 = Menu(1, "Test 1", 10)
+    user_interface = UserInterface(menu_repository)
     order1 = Order(1, 10)
     order2 = Order(2, 10)
 
     # Act
     menu_repository.set_menu_item(menu1)
-    total_price1 = menu_repository.get_total_price(order1)
-    total_price2 = menu_repository.get_total_price(order2)
+    total_price1 = user_interface.get_total_price(order1)
+    total_price2 = user_interface.get_total_price(order2)
 
     # Assert
     assert total_price1 == 100
     assert total_price2 == None
+
+
+def test_get_user_input():
+    # Arrange
+    menu_repository = MenuRepository()
+    user_interface = UserInterface(menu_repository)
+    test_order = Order(1, 10)
+
+    # Act
+    order = user_interface.get_user_input()
+
+    # Assert
+    assert order.code == test_order.code
+    assert order.quantity == test_order.quantity
